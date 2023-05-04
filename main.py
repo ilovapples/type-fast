bb1ff191ce82f694f94f6bd84cb3e1de16cdfc34
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-05-04 10:34:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-from getkey import getkey
-from pytermgui import tim
+# from getkey import getkey
+# from pytermgui import tim
+
+# def menu():
+#   tim.print()
+
+# def main():
+  
+  
+# if __name__ == "__main__":
+#   main()
